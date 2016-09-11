7750e673dea788d1bef83b2d511095358cb35fb4
Tarandeep Singh <tds.max@gmail.com>
2016-09-11 14:24:14 +0530

    Formatted Code

--- src/jsx/index.jsx
+++ src/jsx/index.jsx
@@ -51,6 +51,8 @@ Sys = {
     }
 };
 
+Sys.isObject = Sys.isArguments = Sys.isFunction = Sys.isString = Sys.isArray = Sys.isUndefined = "";
+
 // This Method will create Multiple functions in the Sys object that can be used to test type of
 ['Arguments', 'Function', 'String', 'Number', 'Date', 'RegExp', 'Object', 'Array', 'Undefined']
     .forEach(
@@ -63,193 +65,192 @@ Sys = {
 
 /**
  * @description Ajax Module
+ * @author Tarandeep Singh
+ * @created 2016-09-07
  */
-(function (global, undef) {
 
-	// Define our constructor
-	"use strict";
-	var HP = global.HP = global.HP||{};
-	
-	HP.ajax = function(options) {
+/* Define our constructor*/
+"use strict";
+var HP = window.HP = window.HP||{};
 
-		// Define option defaults
-		var defaults = {
-		  type:'GET',
-		  url: "",
-		  dataType: 'application/json',
-		  contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
-		  cache:true,
-		  data :"",
-		  async:true,
-		  success: "",
-		  error : ""
-		};
+HP.ajax = function(options) {
 
-		// Create options by extending defaults with the passed in arugments
-		if (Sys.isDefined(arguments[0]) && Sys.isObject(arguments[0])) {
-			options = Sys.extend(defaults, arguments[0]);
-		}
+    // Define option defaults
+    var defaults = {
+        type: 'GET',
+        url: "",
+        dataType: 'application/json',
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+        cache: true,
+        data: "",
+        async: true,
+        success: "",
+        error: "",
+        processData: false
+    };
 
-		// Utility method to extend defaults with user options
-		var _construction = function(){
-		
-			if (Sys.isUndefined(XMLHttpRequest)){
-				XMLHttpRequest = function(){
-					try {
-						return new ActiveXObject('Msxml2.XMLHTTP.6.0');
-					}
-					catch (e){}
-					try {
-						return new ActiveXObject('Msxml2.XMLHTTP.3.0');
-					}
-					catch (e){}
-					try {
-						return new ActiveXObject('Msxml2.XMLHTTP')
-					}
-					catch (e){}
-					
-					throw new Error ('this browser does not support XMLHttpRequest.')
-				}
-			}
-			
-			
-			var xmlhttp = new XMLHttpRequest();
-			
-			xmlhttp.open(options.type, options.url);
-			
-			xmlhttp.onreadystatechange = function() {
-				/*try{*/
-	                if(xmlhttp.readyState === 4 &&  xmlhttp.status === 200){
-	                	var response = "";
-	                	if(xmlhttp.responseText == "true"){
-							response = true;
-	                	}
-	                	else if(xmlhttp.responseText == "false"){
-	                		response = false;
-	                	}
-	                	else{
-	                		response = xmlhttp.responseText;
-	                	}
-						options.success(response);
-	                }
-	                else if(xmlhttp.readyState === 4 && xmlhttp.status !== 200){
-	                	if(options.error){
-	                		options.error(xmlhttp, xmlhttp.status, xmlhttp.responseText);
-	                	}
-	                	else{
-	                		console.log("Error in Ajax Call Not Handled");
-	                	}
-	                }	                
-			  /* } catch (e) {
-					if(options.error){
-						options.error(xmlhttp, 900, e.message);
-					}
-					else{
-						console.log("Caught Exception in Ajax Not Handled");
-					}
+    // Create options by extending defaults with the passed in arugments
+    if (Sys.isDefined(arguments[0]) && Sys.isObject(arguments[0])) {
+        options = Sys.extend(defaults, arguments[0]);
+    }
 
-				}*/
+    // Utility method to extend defaults with user options
+    var _construction = function () {
 
-			};
+        if (Sys.isUndefined(XMLHttpRequest)) {
+            XMLHttpRequest = function () {
+                try {
+                    return new ActiveXObject('Msxml2.XMLHTTP.6.0');
+                }
+                catch (e) {
+                }
+                try {
+                    return new ActiveXObject('Msxml2.XMLHTTP.3.0');
+                }
+                catch (e) {
+                }
+                try {
+                    return new ActiveXObject('Msxml2.XMLHTTP')
+                }
+                catch (e) {
+                }
 
-			// Support For datatType as JSON
-			if(options.dataType == 'json'){
-				options.dataType = 'application/json';
-			}
+                throw new Error('this browser does not support XMLHttpRequest.')
+            }
+        }
 
-			// Process Data in Ajax 
-			if(Sys.isObject(options.data) && options.processData !== false){
-				var urldata = "";
-				for(var key in options.data){
-					urldata += key+'='+options.data[key]+'&';
-				}
-				options.data = urldata.slice(0,-1);
-			}
 
-			// Get AJAX
-			if(options.type === "GET"){
-				if(Sys.isDefined(options.data) && options.data !=""){
-					var url = (options.url.indexOf('?') != -1)?options.url + "&" + options.data: options.url + "?" + options.data;
-				} else{
-					var url = options.url;
-				}
-				xmlhttp.open(options.type, url, true);
-				xmlhttp.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-				xmlhttp.send();
-			}else if(options.type === "POST"){
-				xmlhttp.open(options.type, options.url, options.async);
-				xmlhttp.setRequestHeader("Accept",options.dataType);
-				xmlhttp.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-				xmlhttp.setRequestHeader("Content-Type",options.contentType);
-				xmlhttp.send(options.data);
-			}else {
-				console.log("Ajax type not Known : Please Handle Accordinly ");
-			}
-			
-		};
-		
-		var _init = function(){
-			_construction();
-		};
-		
-		_init();
-	}
-		
+        var xmlhttp = new XMLHttpRequest();
 
-}(window));
+        xmlhttp.open(options.type, options.url);
+
+        xmlhttp.onreadystatechange = function () {
+            try {
+                if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+                    var response = "";
+                    if (xmlhttp.responseText == "true") {
+                        response = true;
+                    }
+                    else if (xmlhttp.responseText == "false") {
+                        response = false;
+                    }
+                    else {
+                        response = xmlhttp.responseText;
+                    }
+                    options.success(response);
+                }
+                else if (xmlhttp.readyState === 4 && xmlhttp.status !== 200) {
+                    if (options.error) {
+                        options.error(xmlhttp, xmlhttp.status, xmlhttp.responseText);
+                    }
+                    else {
+                        console.log("Error in Ajax Call Not Handled");
+                    }
+                }
+            } catch (e) {
+                if (options.error) {
+                    options.error(xmlhttp, 900, e.message);
+                }
+                else {
+                    console.log("Caught Exception in Ajax Not Handled");
+                }
+
+            }
+
+        };
+
+        // Support For data Type as JSON
+        if (options.dataType == 'json') {
+            options.dataType = 'application/json';
+        }
+
+        // Process Data in Ajax
+        if (Sys.isObject(options.data) && options.processData !== false) {
+            var urldata = "";
+            for (var key in options.data) {
+                urldata += key + '=' + options.data[key] + '&';
+            }
+            options.data = urldata.slice(0, -1);
+        }
+
+        // Get AJAX
+        if (options.type === "GET") {
+            var url = "";
+            if (Sys.isDefined(options.data) && options.data != "") {
+                url = (options.url.indexOf('?') != -1) ? options.url + "&" + options.data : options.url + "?" + options.data;
+            } else {
+                url = options.url;
+            }
+            xmlhttp.open(options.type, url, true);
+            xmlhttp.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+            xmlhttp.send();
+        } else if (options.type === "POST") {
+            xmlhttp.open(options.type, options.url, options.async);
+            xmlhttp.setRequestHeader("Accept", options.dataType);
+            xmlhttp.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+            xmlhttp.setRequestHeader("Content-Type", options.contentType);
+            xmlhttp.send(options.data);
+        } else {
+            console.log("Ajax type not Known : Please Handle Accordinly ");
+        }
+
+    };
+    _construction();
+};
+
 
 var container = "hotelsContainer";
 var containerElem = document.getElementById("hotelsContainer");
 var size = 10;
 var scrollCount = -1;
-// Handle Ajax Calls Here 
+// Handle Ajax Calls Here
 var urls = {
-	hotels: "http://fake-hotel-api.herokuapp.com/api/hotels",
-	reviews: "http://fake-hotel-api.herokuapp.com/api/reviews"
-}
+    hotels: "http://fake-hotel-api.herokuapp.com/api/hotels",
+    reviews: "http://fake-hotel-api.herokuapp.com/api/reviews"
+};
 
 HP.ajax({
-	url: urls.hotels + "?&count=" + size,
-	success: function(res){
-		res = JSON.parse(res);
-		_renderHotel(res,++scrollCount);
-	},
-	error: function(res){
-		console.log(res);
-	}
+    url: urls.hotels + "?&count=" + size,
+    success: function (res) {
+        res = JSON.parse(res);
+        _renderHotel(res, ++scrollCount);
+    },
+    error: function (res) {
+        console.log(res);
+    }
 });
 
 var Hotel = React.createClass({
-	render: function(){
-		var name = this.props.data.name;
-		return (
-			<li>{name}</li>
-		);
-	}
+    render: function () {
+        var name = this.props.data.name;
+        return (
+            <li>{name}</li>
+        );
+    }
 });
 var HolidayHotels = React.createClass({
-  render: function() {
-  	var hotels = this.props.data.map(function(hotel,index){
-    				return (
-    					<Hotel key={index} data={hotel}></Hotel>
-					);
-    			});
-    return (
-    	<ul>{hotels}</ul>
-	);
-  }
+    render: function () {
+        var hotels = this.props.data.map(function (hotel, index) {
+            return (
+                <Hotel key={index} data={hotel}></Hotel>
+            );
+        });
+        return (
+            <ul>{hotels}</ul>
+        );
+    }
 });
 
- function _renderHotel(data,scrollCount){
-	var id = container+scrollCount;
-	if(!document.getElementById(id)){
-		var elem = document.createElement("div");
-		elem.id = id;
-		elem.class = "hotel";
-		containerElem.appendChild(elem);
-	}
-	ReactDOM.render(
-	  <HolidayHotels data={data}/>,
-	  document.getElementById(id)
-	);	
+function _renderHotel(data, scrollCount) {
+    var id = container + scrollCount;
+    if (!document.getElementById(id)) {
+        var elem = document.createElement("div");
+        elem.id = id;
+        elem.class = "hotel";
+        containerElem.appendChild(elem);
+    }
+    ReactDOM.render(
+        <HolidayHotels data={data}/>,
+        document.getElementById(id)
+    );
 }
